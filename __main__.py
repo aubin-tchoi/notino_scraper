61a4cdb3e034021fedd8f7731aaa3de8c7053b3d
aubin-tchoi <aubin.tchoi@gmail.com>
2021-12-30 19:35:11 +0100

    added the command line part

--- __main__.py
+++ __main__.py
@@ -1,5 +1,22 @@
 import getopt
-
+import sys
+from notino_scraper.notino_scraper import NotinoScraper
 
 if __name__ == '__main__':
-    print("Hello")
+    argv = sys.argv[1:]
+    opts, args = getopt.getopt(argv, "pso:a:", ["print", "take_snapshot", "output_file=", "add_product="])
+
+    for opt, arg in opts:
+        if opt in ("-o", "--output"):
+            NotinoScraper.update_datafile(arg)
+            break
+
+    notino_scraper = NotinoScraper()
+
+    for opt, arg in opts:
+        if opt in ("-p", "--print"):
+            print(notino_scraper.product_list)
+        if opt in ("-s", "--take_snapshot"):
+            notino_scraper.take_snapshot()
+        if opt in ("-a", "--add_product"):
+            notino_scraper.add_product(arg)
