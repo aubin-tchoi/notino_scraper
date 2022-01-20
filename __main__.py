b2333af484744b7429411230c7d8b1a70e5e16c1
aubin-tchoi <aubin.tchoi@gmail.com>
2022-01-20 23:59:14 +0100

    changed the order in the command line options

--- __main__.py
+++ __main__.py
@@ -20,25 +20,25 @@ if __name__ == '__main__':
         if opt in ("-o", "--output"):
             NotinoScraper.update_datafile(arg)
             exit()
-        if opt in ("-v", "--verbose"):
-            verbose = arg not in ["False", "false", "N", "n"]
         if opt in ("-c", "--set_config"):
             NotinoScraper.set_config_parameters()
             exit()
+        if opt in ("-v", "--verbose"):
+            verbose = arg not in ["False", "false", "N", "n"]
 
     # This instantiation takes some time.
     notino_scraper = NotinoScraper(verbose)
 
     for opt, arg in opts:
+        if opt in ("-p", "--print"):
+            print(notino_scraper.product_list)
         if opt in ("-s", "--take_snapshot"):
             notino_scraper.take_snapshot()
+        if opt in ("-g", "--plot"):
+            notino_scraper.plot_evolution()
         if opt in ("-a", "--add_products"):
             for product_name in arg.split("; "):
                 notino_scraper.add_product(product_name)
-        if opt in ("-p", "--print"):
-            print(notino_scraper.product_list)
-        if opt in ("-g", "--plot"):
-            notino_scraper.plot_evolution()
         if opt in ("-f", "--get_prices"):
             for search_name in arg.split("; "):
                 notino_scraper.get_price(search_name)
