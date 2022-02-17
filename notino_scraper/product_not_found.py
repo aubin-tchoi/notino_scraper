8324e0c087bec2dd985213cdcbb28c49dcdc8bd0
aubin-tchoi <aubin.tchoi@gmail.com>
2022-02-18 00:06:00 +0100

    initial commit

---
+++ notino_scraper/product_not_found.py
@@ -0,0 +1,8 @@
+
+
+class ProductNotFoundException(Exception):
+
+    def __init__(self, product: str, message: str = "Product not found.") -> None:
+        self.product = product
+        self.message = message
+        super().__init__(self.message)
