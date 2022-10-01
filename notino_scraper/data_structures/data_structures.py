083744c8a30bccb9a661624bc2dbcc0c4d23e596
aubin-tchoi <aubin.tchoi@gmail.com>
2022-10-01 13:53:24 +0200

    added docstrings

--- notino_scraper/data_structures/data_structures.py
+++ notino_scraper/data_structures/data_structures.py
@@ -6,8 +6,19 @@ from typing import List, TypedDict, Optional
 @dataclass
 class ProductPrice:
     price: Optional[float] = None
+    """
+    Price of the product.
+    It is null if either the product is not available for a specific volume or it is not available at all.
+    """
     volume: int = 0
+    """
+    Volume of the product in mL.
+    It is equal to 0 iff the product is not available at all.
+    """
     date: str = date.today().isoformat()
+    """
+    Date of the snapshot in format YYYY-MM-DD.
+    """
 
 
 class ProductInfo(TypedDict):
